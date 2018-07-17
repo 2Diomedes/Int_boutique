3b8bb0ac696641999fbb2051d3901c74e2cb0818
Mordnor <dorian.pitiot@gmail.com>
2018-07-17 16:31:12 +0200

    utilisation nouveau catalog

--- js/script.js
+++ js/script.js
@@ -1,20 +1,37 @@
 $(document).ready(function() {
 
+console.log(catalog);
 
 
-    for (var i = 0; i < 18 ; i++) {
+    for (var i = 0; i < 36 ; i++) {
 
         $(".main_article").append(`<div class="col-12 col-sm-6 col-md-3 p-4 col-xl-2 products">
-            <div class="card">
+            <div class="card" id="card`+[i]+`">
             <img class="card-img-top" src="http://placehold.it/80x100" alt="Card image cap">
             <div class="card-body">
-            <h4>on sale</h4>
+            <h4 id="product`+[i]+`">on sale</h4>
             <h5 class="card-title">${catalog[i].name}</h5>
             <p class="card-text">${catalog[i].price} €</p>
             </div>
             </div>
             </div>
         `)
+
+        if (catalog[i]["onSale"] == true) {
+            $("#product"+i+"").text("En Solde").css("color", "blue")
+        }
+
+        else if (catalog[i].quantity == 0) {
+            $("#product"+i+"").text("Hors Stock").css("color", "red")
+            var rupture = $("<hr>")
+            $("#card"+i+"").append(rupture)
+            $(rupture).css("border", "2px solid red")
+
+        }
+
+        else if (catalog[i].quantity > 0) {
+            $("#product"+i+"").text("Buy").css("color", "green")
+        }
     }
 
 
